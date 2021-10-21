0f5ceda3e92f47369512f1bf5aba6a6a1c0c23a4
fadhilthomas <mthomasfadhilayahya@gmail.com>
2021-10-21 20:03:21 +0000

    feat: update read json

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"github.com/fadhilthomas/go-code-scanning-reporter/config"
 	"github.com/fadhilthomas/go-code-scanning-reporter/model"
@@ -9,6 +8,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/rs/zerolog/pkgerrors"
+	"io/ioutil"
 	"os"
 	"strconv"
 )
@@ -61,151 +61,149 @@ func main() {
 		return
 	}
 
-	fScanner := bufio.NewScanner(fileReport)
-	for fScanner.Scan() {
-		var vulnerabilityList []model.Vulnerability
-		switch {
-		case scanType == "scan-secret":
-			err = json.Unmarshal([]byte(fScanner.Text()), &scanSecretReport)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			for _, scanSecret := range scanSecretReport {
-				vulnerability := model.Vulnerability{}
-				vulnerability.Name = scanSecret.Rule
-				vulnerability.Path = scanSecret.File
-				vulnerability.Detail = float64(scanSecret.LineNumber)
-				vulnerabilityList = append(vulnerabilityList, vulnerability)
-			}
-		case scanType == "scan-dependency-go":
-			err = json.Unmarshal([]byte(fScanner.Text()), &scanDependencyGoReport)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			for _, scanDependencyGo := range scanDependencyGoReport.Vulnerable {
-				for _, scanDependencyGoDetail := range scanDependencyGo.Vulnerabilities {
-					vulnerability := model.Vulnerability{}
-					vulnerability.Name = scanDependencyGoDetail.Title
-					vulnerability.Path = scanDependencyGo.Coordinates
-					vulnerability.Detail, _ = strconv.ParseFloat(scanDependencyGoDetail.CvssScore, 64)
-					vulnerabilityList = append(vulnerabilityList, vulnerability)
-				}
-			}
-		case scanType == "scan-dependency-js":
-			err = json.Unmarshal([]byte(fScanner.Text()), &scanDependencyJsReport)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			for _, scanDependencyJs := range scanDependencyJsReport {
-				for _, scanDependencyJsDetail := range scanDependencyJs.Vulnerabilities {
-					vulnerability := model.Vulnerability{}
-					vulnerability.Name = scanDependencyJsDetail.Title
-					vulnerability.Path = scanDependencyJs.Coordinates
-					vulnerability.Detail = scanDependencyJsDetail.CvssScore
-					vulnerabilityList = append(vulnerabilityList, vulnerability)
-				}
-			}
-		case scanType == "scan-dependency-php":
-			err = json.Unmarshal([]byte(fScanner.Text()), &scanDependencyPhpReport)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			for _, scanDependencyPhp := range scanDependencyPhpReport {
-				for _, scanDependencyPhpDetail := range scanDependencyPhp.Vulnerabilities {
-					vulnerability := model.Vulnerability{}
-					vulnerability.Name = scanDependencyPhpDetail.Title
-					vulnerability.Path = scanDependencyPhp.Coordinates
-					vulnerability.Detail = scanDependencyPhpDetail.CvssScore
-					vulnerabilityList = append(vulnerabilityList, vulnerability)
-				}
-			}
-		case scanType == "scan-security-static-code-codeql":
-			err = json.Unmarshal([]byte(fScanner.Text()), &scanSecurityStaticCodeCodeQlReport)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			for _, scanSecurityStaticCodeCodeQl := range scanSecurityStaticCodeCodeQlReport.Runs {
-				for _, scanSecurityStaticCodeCodeQlDetail := range scanSecurityStaticCodeCodeQl.Results {
-					vulnerability := model.Vulnerability{}
-					vulnerability.Name = scanSecurityStaticCodeCodeQlDetail.RuleID
-					vulnerability.Path = scanSecurityStaticCodeCodeQlDetail.Locations[0].PhysicalLocation.ArtifactLocation.URI
-					vulnerability.Detail = float64(scanSecurityStaticCodeCodeQlDetail.Locations[0].PhysicalLocation.Region.StartLine)
-					vulnerabilityList = append(vulnerabilityList, vulnerability)
-				}
-			}
-		case scanType == "scan-security-static-code-semgrep":
-			err = json.Unmarshal([]byte(fScanner.Text()), &scanSecurityStaticCodeSemgrepReport)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			for _, scanSecurityStaticCodeSemgrep := range scanSecurityStaticCodeSemgrepReport {
-				vulnerability := model.Vulnerability{}
-				vulnerability.Name = scanSecurityStaticCodeSemgrep.CheckID
-				vulnerability.Path = scanSecurityStaticCodeSemgrep.Path
-				vulnerability.Detail = float64(scanSecurityStaticCodeSemgrep.Line)
-				vulnerabilityList = append(vulnerabilityList, vulnerability)
-			}
-		}
-
-		// loop vuln struct
-		for _, vulnerability := range vulnerabilityList {
-			// search vuln in notion
-			notionQueryNameResult, err := model.QueryNotionVulnerabilityName(notionDatabase, scanType, repositoryName, vulnerability.Name, vulnerability.Path, vulnerability.Detail)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-
-			// if no result, insert vuln to notion. if not append the notion page list
-			if len(notionQueryNameResult) == 0 {
-				_, err = model.InsertNotionVulnerability(notionDatabase, scanType, repositoryName, repositoryPullRequest, vulnerability.Name, vulnerability.Path, vulnerability.Detail)
-				if err != nil {
-					log.Error().Stack().Err(err).Msg("")
-					return
-				}
-				summaryReportStatus.New++
-			} else {
-				notionPageList = append(notionPageList, notionQueryNameResult[0])
-			}
-
-			// loop notion page list, set status to open
-			for _, notionPage := range notionPageList {
-				_, err = model.UpdateNotionVulnerabilityStatus(notionDatabase, notionPage.ID.String(), "open")
-				if err != nil {
-					log.Error().Stack().Err(err).Msg("")
-					return
-				}
-			}
-
-			summaryReportStatus.Open = len(vulnerabilityList)
-
-			// find all close entries in repository
-			notionQueryStatusResult, err = model.QueryNotionVulnerabilityStatus(notionDatabase, repositoryName, "close")
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-			summaryReportStatus.Close = len(notionQueryStatusResult)
-		}
-		summaryReportStatus.RepositoryPullRequest = repositoryPullRequest
-		summaryReportStatus.ScanType = scanType
-
-		if summaryReportStatus.New == 0 {
-			return
-		}
-
-		var slackBlockList []model.SlackBlockBody
-		slackBlockList = append(slackBlockList, model.CreateBlockSummary(summaryReportStatus))
-		err = model.SendSlackNotification(slackToken, slackBlockList)
+	byteValue, _ := ioutil.ReadAll(fileReport)
+	var vulnerabilityList []model.Vulnerability
+	switch {
+	case scanType == "scan-secret":
+		err = json.Unmarshal(byteValue, &scanSecretReport)
 		if err != nil {
 			log.Error().Stack().Err(err).Msg("")
 			return
 		}
+		for _, scanSecret := range scanSecretReport {
+			vulnerability := model.Vulnerability{}
+			vulnerability.Name = scanSecret.Rule
+			vulnerability.Path = scanSecret.File
+			vulnerability.Detail = float64(scanSecret.LineNumber)
+			vulnerabilityList = append(vulnerabilityList, vulnerability)
+		}
+	case scanType == "scan-dependency-go":
+		err = json.Unmarshal(byteValue, &scanDependencyGoReport)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+		for _, scanDependencyGo := range scanDependencyGoReport.Vulnerable {
+			for _, scanDependencyGoDetail := range scanDependencyGo.Vulnerabilities {
+				vulnerability := model.Vulnerability{}
+				vulnerability.Name = scanDependencyGoDetail.Title
+				vulnerability.Path = scanDependencyGo.Coordinates
+				vulnerability.Detail, _ = strconv.ParseFloat(scanDependencyGoDetail.CvssScore, 64)
+				vulnerabilityList = append(vulnerabilityList, vulnerability)
+			}
+		}
+	case scanType == "scan-dependency-js":
+		err = json.Unmarshal(byteValue, &scanDependencyJsReport)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+		for _, scanDependencyJs := range scanDependencyJsReport {
+			for _, scanDependencyJsDetail := range scanDependencyJs.Vulnerabilities {
+				vulnerability := model.Vulnerability{}
+				vulnerability.Name = scanDependencyJsDetail.Title
+				vulnerability.Path = scanDependencyJs.Coordinates
+				vulnerability.Detail = scanDependencyJsDetail.CvssScore
+				vulnerabilityList = append(vulnerabilityList, vulnerability)
+			}
+		}
+	case scanType == "scan-dependency-php":
+		err = json.Unmarshal(byteValue, &scanDependencyPhpReport)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+		for _, scanDependencyPhp := range scanDependencyPhpReport {
+			for _, scanDependencyPhpDetail := range scanDependencyPhp.Vulnerabilities {
+				vulnerability := model.Vulnerability{}
+				vulnerability.Name = scanDependencyPhpDetail.Title
+				vulnerability.Path = scanDependencyPhp.Coordinates
+				vulnerability.Detail = scanDependencyPhpDetail.CvssScore
+				vulnerabilityList = append(vulnerabilityList, vulnerability)
+			}
+		}
+	case scanType == "scan-security-static-code-codeql":
+		err = json.Unmarshal(byteValue, &scanSecurityStaticCodeCodeQlReport)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+		for _, scanSecurityStaticCodeCodeQl := range scanSecurityStaticCodeCodeQlReport.Runs {
+			for _, scanSecurityStaticCodeCodeQlDetail := range scanSecurityStaticCodeCodeQl.Results {
+				vulnerability := model.Vulnerability{}
+				vulnerability.Name = scanSecurityStaticCodeCodeQlDetail.RuleID
+				vulnerability.Path = scanSecurityStaticCodeCodeQlDetail.Locations[0].PhysicalLocation.ArtifactLocation.URI
+				vulnerability.Detail = float64(scanSecurityStaticCodeCodeQlDetail.Locations[0].PhysicalLocation.Region.StartLine)
+				vulnerabilityList = append(vulnerabilityList, vulnerability)
+			}
+		}
+	case scanType == "scan-security-static-code-semgrep":
+		err = json.Unmarshal(byteValue, &scanSecurityStaticCodeSemgrepReport)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+		for _, scanSecurityStaticCodeSemgrep := range scanSecurityStaticCodeSemgrepReport {
+			vulnerability := model.Vulnerability{}
+			vulnerability.Name = scanSecurityStaticCodeSemgrep.CheckID
+			vulnerability.Path = scanSecurityStaticCodeSemgrep.Path
+			vulnerability.Detail = float64(scanSecurityStaticCodeSemgrep.Line)
+			vulnerabilityList = append(vulnerabilityList, vulnerability)
+		}
+	}
+
+	// loop vuln struct
+	for _, vulnerability := range vulnerabilityList {
+		// search vuln in notion
+		notionQueryNameResult, err := model.QueryNotionVulnerabilityName(notionDatabase, scanType, repositoryName, vulnerability.Name, vulnerability.Path, vulnerability.Detail)
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+
+		// if no result, insert vuln to notion. if not append the notion page list
+		if len(notionQueryNameResult) == 0 {
+			_, err = model.InsertNotionVulnerability(notionDatabase, scanType, repositoryName, repositoryPullRequest, vulnerability.Name, vulnerability.Path, vulnerability.Detail)
+			if err != nil {
+				log.Error().Stack().Err(err).Msg("")
+				return
+			}
+			summaryReportStatus.New++
+		} else {
+			notionPageList = append(notionPageList, notionQueryNameResult[0])
+		}
+
+		// loop notion page list, set status to open
+		for _, notionPage := range notionPageList {
+			_, err = model.UpdateNotionVulnerabilityStatus(notionDatabase, notionPage.ID.String(), "open")
+			if err != nil {
+				log.Error().Stack().Err(err).Msg("")
+				return
+			}
+		}
+
+		summaryReportStatus.Open = len(vulnerabilityList)
+
+		// find all close entries in repository
+		notionQueryStatusResult, err = model.QueryNotionVulnerabilityStatus(notionDatabase, repositoryName, "close")
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("")
+			return
+		}
+		summaryReportStatus.Close = len(notionQueryStatusResult)
+	}
+	summaryReportStatus.RepositoryPullRequest = repositoryPullRequest
+	summaryReportStatus.ScanType = scanType
+
+	if summaryReportStatus.New == 0 {
+		return
+	}
+
+	var slackBlockList []model.SlackBlockBody
+	slackBlockList = append(slackBlockList, model.CreateBlockSummary(summaryReportStatus))
+	err = model.SendSlackNotification(slackToken, slackBlockList)
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("")
+		return
 	}
 }
